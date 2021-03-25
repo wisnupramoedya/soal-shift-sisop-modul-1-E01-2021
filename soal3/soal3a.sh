b0afae9f6e1c591544af4e355497c4cf598a333f
Wisnu Pramoedya <wisnupramoedya@gmail.com>
2021-03-25 22:02:25 +0700

    Fix: soal3a

---
+++ soal3/soal3a.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+for i in {1..23}
+do
+    wget --trust-server-names -a "Foto.log" https://loremflickr.com/320/240/kitten
+done
+
+img_array_duplicate=($(ls *.jpg.*))
+
+img_array=($(ls *.jpg))
+
+for i in "${img_array_duplicate[@]}"
+do
+    rm $i
+    # echo $i
+done
+
+for i in "${!img_array[@]}"
+do
+    if [[ $i -lt 9 ]]
+    then
+        mv "${img_array[$i]}" "Koleksi_0$((i+1)).jpg"
+        echo "${img_array[$i]}" 
+    else
+        mv "${img_array[$i]}" "Koleksi_$((i+1)).jpg"
+    fi
+    echo $i
+done
